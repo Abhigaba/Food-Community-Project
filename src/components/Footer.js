194f87a13a2e536072828f4758bacc336d209537
Abhigaba <99534945+Abhigaba@users.noreply.github.com>
2023-11-30 22:37:53 +0530

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <div className="foo">
+      <footer>
+  Joker's Foodary © 2023
+  
+    </footer>
+    </div>
+  );
+}
+export default Footer;
---
+++ src/recipe.js
@@ -0,0 +1,41 @@
+export const my_recipe = {
+    recipe_1: {
+      name: 'Chirashi Don',
+      preparation_time: '30 minutes',
+      image: <img src = "/img/recipe1.jpg" alt='' className="recipe-image"></img>,
+      servings: '2',
+      ingredients: [
+        '1/2 cup rice',
+        '1/2 cup water',
+        '2 eggs',
+        '300g of salmon, tuna and salmon roe',
+      ],
+      instructions: [
+        'Cook rice and water in a rice cooker',
+        'Fry eggs in a pan',
+        'Serve rice, eggs and fish on a plate',
+      ],
+   
+    },
+    recipe_2: {
+        name: 'Coffee',
+        preparation_time: '5 minutes',
+        image: <img src = "/img/recipe2.jpg" alt="" className="recipe-image"></img>,
+        servings: '2',
+        ingredients: [
+          '1/2 tbsp coffee',
+          'Warm water',
+          '2 cup milk ',
+          'Sugar if needed',
+        ],
+        instructions: [
+          'In a glass or a cup, take the coffee powder and sugar according to required',
+          'Add enough warm water to the cup such that the end result isnt liquidy',
+          'Now grind the mixture add while adding warm water.',
+          'Repeat the above process till the sugar dissolves',
+          'Now heat up the milk and the mixture to a cup acc. to requirement ',
+          'Add the hot milk to that cup and enjoy the coffee with nice froath',
+        ],
+     
+      },
+  }
